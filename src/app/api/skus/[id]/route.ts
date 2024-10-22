d95dc7b92ad2d1455200f85eb098fbef55f0d940
Kharl <enriquezkharl14@gmail.com>
2024-10-23 00:40:06 +0800

    [Feat] Added update sku route

---
+++ src/app/api/skus/[id]/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { SKUSchema } from '@/schemas/sku';
+
+import prisma from '../../../../../prisma/db';
+
+interface Props {
+  params: { id: string };
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+
+  const validation = SKUSchema.safeParse(body);
+
+  if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const sku = await prisma.sKU.findUnique({
+    where: {
+      id: parseInt(params.id)
+    }
+  });
+
+  if (!sku) return NextResponse.json({ error: 'SKU not found!' }, { status: 404 });
+
+  const updatedSKU = await prisma.sKU.update({
+    where: {
+      id: parseInt(params.id)
+    },
+    data: {
+      ...body
+    }
+  });
+
+  return NextResponse.json(updatedSKU);
+}
